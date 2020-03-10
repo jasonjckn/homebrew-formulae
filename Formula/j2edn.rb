43fd88f766a8cced71eed4cc99f9a5806de42fbb
Jason Jackson <jasonjckn@gmail.com>
2020-03-10 03:25:01 -0700

    x

--- Formula/j2edn.rb
+++ Formula/j2edn.rb
@@ -15,13 +15,14 @@ class J2edn < Formula
   head "https://github.com/jasonjckn/j2edn.git"
   bottle :unneeded
 
+  ENV["GRAALVM_HOME"] =  "/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.0.0/Contents/Home"
+  ENV.prepend_path "PATH", Pathname.new(ENV["GRAALVM_HOME"])/"bin/"
+
   uses_from_macos "ruby" => :build
   depends_on "clojure/tools/clojure" => :build
   depends_on RRequirement
 
   def install
-    ENV["GRAALVM_HOME"] =  "/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.0.0/Contents/Home"
-    ENV.prepend_path "PATH", Pathname.new(ENV["GRAALVM_HOME"])/"bin/"
     system "sudo", "-u", "jason", "gu", "install", "-f", "native-image"
     system "clojure", "-A:native-image"
     system "mkdir", prefix/"bin"
