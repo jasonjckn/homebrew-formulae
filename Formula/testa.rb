faeaa6a0b306d757646cea90144cc640a6b0dc11
Jason Jackson <jasonjckn@gmail.com>
2020-03-10 01:40:50 -0700

    x

--- Formula/testa.rb
+++ Formula/testa.rb
@@ -1,4 +1,4 @@
-class TestA < Formula
+class Testa < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
   url "https://download.clojure.org/install/clojure-tools-1.10.1.536.tar.gz"
