3d31b357b857836a1d5e188eac2535611b754431
Jason Jackson <jasonjckn@gmail.com>
2020-03-10 03:25:58 -0700

    x

--- Formula/j2edn.rb
+++ Formula/j2edn.rb
@@ -1,7 +1,11 @@
 class RRequirement < Requirement
   fatal true
 
-  satisfy(:build_env => false) { which("native-image2") }
+  satisfy(:build_env => false) {
+    ENV["GRAALVM_HOME"] =  "/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.0.0/Contents/Home"
+    ENV.prepend_path "PATH", Pathname.new(ENV["GRAALVM_HOME"])/"bin/"
+    which("native-image")
+  }
 
   def message; <<~EOS
     GraalVM + native-image is required; install it via one of:
@@ -15,15 +19,15 @@ class J2edn < Formula
   head "https://github.com/jasonjckn/j2edn.git"
   bottle :unneeded
 
-  ENV["GRAALVM_HOME"] =  "/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.0.0/Contents/Home"
-  ENV.prepend_path "PATH", Pathname.new(ENV["GRAALVM_HOME"])/"bin/"
-
   uses_from_macos "ruby" => :build
   depends_on "clojure/tools/clojure" => :build
   depends_on RRequirement
 
   def install
-    system "sudo", "-u", "jason", "gu", "install", "-f", "native-image"
+    ENV["GRAALVM_HOME"] =  "/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.0.0/Contents/Home"
+    ENV.prepend_path "PATH", Pathname.new(ENV["GRAALVM_HOME"])/"bin/"
+
+    #system "sudo", "-u", "jason", "gu", "install", "-f", "native-image"
     system "clojure", "-A:native-image"
     system "mkdir", prefix/"bin"
     system "cp", "core", prefix/"bin/j2edn"
