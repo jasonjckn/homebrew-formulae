4e6f2d8f671323d9d559fc7a549e928f66378016
Jason Jackson <jasonjckn@gmail.com>
2020-03-10 01:36:42 -0700

    x

---
+++ test.rb
@@ -0,0 +1,31 @@
+class Clojure < Formula
+  desc "The Clojure Programming Language"
+  homepage "https://clojure.org"
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.536.tar.gz"
+  sha256 "b7c5b0cdeb750275ddd98095a1959657b95569b624da7c6163adce5a7d5f7119"
+
+  devel do
+    url "https://download.clojure.org/install/clojure-tools-1.10.1.536.tar.gz"
+    sha256 "b7c5b0cdeb750275ddd98095a1959657b95569b624da7c6163adce5a7d5f7119"
+    version "1.10.1.536"
+  end
+
+  bottle :unneeded
+
+  depends_on "rlwrap"
+
+  uses_from_macos "ruby" => :build
+
+  def install
+    system "echo hi", prefix
+    system "ls .", prefix
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj -e nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -e \"(+ 1 1)\"").strip
+    end
+  end
+end
