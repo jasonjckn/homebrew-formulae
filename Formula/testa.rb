47aa20353cef540007ba6f18396ac66b02e87634
Jason Jackson <jasonjckn@gmail.com>
2020-03-10 01:46:06 -0700

    x

--- Formula/testa.rb
+++ Formula/testa.rb
@@ -1,18 +1,13 @@
 class Testa < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.10.1.536.tar.gz"
-  sha256 "b7c5b0cdeb750275ddd98095a1959657b95569b624da7c6163adce5a7d5f7119"
-
-  devel do
-    url "https://download.clojure.org/install/clojure-tools-1.10.1.536.tar.gz"
-    sha256 "b7c5b0cdeb750275ddd98095a1959657b95569b624da7c6163adce5a7d5f7119"
-    version "1.10.1.536"
-  end
+  head "https://github.com/taylorwood/clojurl.git"
 
   bottle :unneeded
 
   depends_on "rlwrap"
+  depends_on "clojure/tools/clojure"
+  depends_on "graalvm/tap/graalvm-ce-java11"
 
   uses_from_macos "ruby" => :build
 
